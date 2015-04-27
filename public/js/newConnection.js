2299842afada1924fc1ae58aa15e24ba26b955da
cristianKumul <cristian.kumul@gmail.com>
2015-04-27 15:13:06 -0500

    CONF LINK
    Onclick abre una nueva ventana (no funciona en safari)

--- public/js/newConnection.js
+++ public/js/newConnection.js
@@ -19,10 +19,24 @@ socket.on('config:client', function(data) {
 });
 socket.on('load:coords', function(data) {
 	var n = Object.keys(markers).length;
+	//alert(n);
 
-	markerId = data.id;
+	markerId = data.id;//Math.random().toString(16).substring(2,15);
 	if (!(data.id in connects) ) {
 		
+		/*var currentCoord =new  L.LatLng(data.destino.lat,data.destino.lon);//'LatLng('+data.destino.lat+', '+data.destino.lon+')';//new  L.LatLng(data.destino.lat,data.destino.lon);
+		
+		if(currentCoord in coordinates){
+				console.log("Existe");
+				var randomlat = (Math.random() * (0.001 - (-0.001)) + (-0.001));
+				var randomlon = (Math.random() * (0.001 - (-0.001)) + (-0.001));
+				console.log(randomlat);
+				console.log(randomlon);
+				data.destino.lat= data.destino.lat+randomlat;
+				data.destino.lon=data.destino.lon+randomlon;
+				console.log(coordinates);
+				
+			}*/
 		
 		setMarker(data);
 		
@@ -36,34 +50,107 @@ socket.on('load:coords', function(data) {
 			map.fitBounds(group.getBounds(),{padding: [50, 50],maxZoom:15});
 			//console.log( Object.keys(markers).length);
 		}
-		
+		ventasCanal[data.canal-1]+=1;
+		ventasServicio[data.canal-1][data.tipoServicio -1]+=1;
+		updateBoth(checked, data);
 
 	}
 	
-
+	/*else{
+		var lastKey;
+		for(var key in markers){
+		    if(markers.hasOwnProperty(key)){
+		       lastKey = key;
+		       break;
+		    }
+		}
+		//lastKey = Object.keys(markers).reverse()[markers.length];
+		map.removeLayer(markers[lastKey]);
+		delete markers[lastKey];
+		//alert(lastKey);
+		setMarker(data);
+	}*/
 	connects[data.id] = data;
 	connects[data.id].updated = $.now();
 	connects[data.id].timeout = true;
 
+	console.log("Número de markers: "+ Object.keys(connects).length);
 	
 });
 
+function updateBoth(option, data){
+	//console.log("opcion: " + data);
 
-function setMarker(data) {
+	if(option == 2){
+		grafica.datasets[data.canal - 1].points[data.tipoServicio - 1].value += 1;
+		grafica.update();
+	} 
+
+	if(option == 3){
+		grafica.segments[data.canal-1].value += 1;
+		grafica.update();
+	}
+
 	
+}
+
+function uptateGraph(data){
+
+	switch(data.canal) {
+
+		case 1:
+			myRadar.datasets[0].points[data.tipoServicio -1].value += 1;
+			myRadar.update();
+			break;
+		case 2:
+			myRadar.datasets[1].points[data.tipoServicio -1].value += 1;
+			myRadar.update();
+			break;
+		case 3:
+			myRadar.datasets[2].points[data.tipoServicio -1].value += 1;
+			myRadar.update();
+			break;
+
+	}
+}
+function uptateGraphD(data){
+
+	switch(data.canal) {
+
+		case 1:
+			
+			myDoughnutChart.segments[0].value +=1;
+			
+			break;
+		case 2:
+			
+			myDoughnutChart.segments[1].value+=1;
+			
+			break;
+		case 3:
+			
+			myDoughnutChart.segments[2].value+=1;
+			break;
+
+	}
+	myDoughnutChart.update();
+}
+function setMarker(data) {
+	//for (var i = 0; i < data.coords.length; i++) {
 		if(data.tipoServicio!=9){
 			
 			var marker = L.marker([data.destino.lat, data.destino.lon],
 								{ bounceOnAdd: true, bounceOnAddOptions: {duration: 500, height: 50},
 								 icon: L.AwesomeMarkers.icon({icon: icons[data.tipoServicio -1], prefix: 'icon', markerColor: canal[data.canal -1], spin:false}) }).addTo(map);	
 
-		
+			
 			coordinates[marker.getLatLng()] = data.id;
 
 			
+		//var marker = L.marker([data.coords[0].olat, data.coords[0].olng], { icon: yellowIcon}).addTo(map);
 		marker.bindPopup('<p>¡Una venta aquí!<br>Usuario:<b>'+data.tipoServicio+'</b><br>Destino:['+data.destino.lat+','+data.destino.lon+']<br>' + '<a href="#" id="showRoute" data-coords=\'{"olat":"'+data.destino.lon+'","olng":"'+data.destino.lon+'","dlat":"'+data.destino.lon+'","dlng":"'+data.destino.lon+'"}\' onclick="drawPolyline();return false;" >Ver ruta</a></p> ');
-	
-		markers[data.id] = marker;
+		//
+		markers[data.id] = marker;//tmp={olat:'+data.coords[0].olat+',olng:'+data.coords[0].olng+',dlat:'+data.coords[0].dlat+',dlng:'+data.coords[0].dlng+'}
 		map.addLayer(markers[data.id]);
 
 		if(marker.getLatLng() in coordinates){
@@ -75,6 +162,8 @@ function setMarker(data) {
 				newCoord = new L.LatLng(data.destino.lat+randomlat,data.destino.lon+randomlon);
 				marker.setLatLng(newCoord);
 				marker.update();
+				//data.destino.lat= data.destino.lat+randomlat;
+				//data.destino.lon=data.destino.lon+randomlon;
 				//console.log(marker.getLatLng());
 				delete connects[data.id];
 				delete coordinates[markers[data.id].getLatLng()];
@@ -92,7 +181,7 @@ function setMarker(data) {
 
 
 
-		bar([icons[data.tipoServicio -1],canal[data.canal -1]]);
+		
 		}
 		else{
 			var marker2 = L.marker([data.destino[0].lon, data.destino[0].lat], { bounceOnAdd: true, bounceOnAddOptions: {duration: 500, height: 50}, icon: L.AwesomeMarkers.icon({icon: icons[data.tipoServicio -1], prefix: 'icon', markerColor: canal[data.canal -1], spin:false}) }).addTo(map);
@@ -117,13 +206,17 @@ function setMarker(data) {
 				});
 			markers[data.id] = circuito;
 			map.addLayer(markers[data.id]);
-			
+			//alert("Circuito");
 		}
-		
+		bar([icons[data.tipoServicio -1],canal[data.canal -1]]);
+		//grafica(data);
+		//var destmarker = L.marker([data.coords[0].dlat, data.coords[0].dlng], { icon: redIcon}).addTo(map);
+	//}
 }
 
+
 function bar(data){
-	
+	//console.log(data.options.iconUrl);
 	if(myControl.queue.length > 9){
     		myControl.queue.shift();
     	}
@@ -135,13 +228,11 @@ function bar(data){
     	}
     	tabla += '</ul></div></div></nav></div>';
 
-    	
+    	//alert(tabla);
     	myControl._div.innerHTML = tabla;
     	
 }
 
-
-// elimina cada n tiempo el marcador que cumpla la condici
 function intervalTrigger(marker){
 	return window.setInterval(function() {
 		try {opacity = marker.options.opacity;
@@ -158,6 +249,10 @@ setInterval(function() {
 					if (connects[ident].timeout){
 							connects[ident].timeout = false;
 							intervalMarker[ident] = intervalTrigger(markers[ident]);
+							/*setInterval(function() {
+								opacity = markers[ident].options.opacity;
+								markers[ident].setOpacity( opacity- 0.05);
+							},200);*/
 
 					}
 					
